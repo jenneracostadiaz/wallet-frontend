6f91857b5be8a692a980e631ac4c1a40b3acd3d3
Jenner Acosta Diaz <dev@jenner.pe>
2025-02-08 10:40:31 -0500

    chore(balance): chane query by payment_date

--- app/Livewire/Widgets/Balance.php
+++ app/Livewire/Widgets/Balance.php
@@ -52,7 +52,11 @@ class Balance extends Component
             }
         }
 
-        $balances['payments'] = auth()->user()->payments()->where('is_paid', false)->whereMonth('created_at', now()->month)->whereYear('created_at', now()->year)->sum('installment_amount');
+        $balances['payments'] = auth()->user()->payments()
+            ->where('is_paid', false)
+            ->whereMonth('payment_date', now()->month)
+            ->whereYear('payment_date', now()->year)
+            ->sum('installment_amount');
 
         $balances['difference'] = $balances['total'] - $balances['payments'];
 
