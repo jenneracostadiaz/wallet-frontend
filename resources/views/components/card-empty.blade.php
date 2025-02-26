42c4a8e7907fb7e36d6392d5414a1e8440a322c0
Jenner Acosta Diaz <dev@jenner.pe>
2025-02-25 20:50:29 -0500

    chore(empty): add card-empty

---
+++ resources/views/components/card-empty.blade.php
@@ -0,0 +1,4 @@
+<div class="flex flex-col items-center justify-center p-4 border-2 border-dashed border-gray-300 rounded-lg">
+    <h2 class="text-lg font-semibold text-gray-600">{{ $title }}</h2>
+    <a href="{{ $link }}" class="mt-4 text-sm text-blue-200 hover:underline">{{ $message }}</a>
+</div>
