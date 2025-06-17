7172d9f56571d3ca9f4d8e1de9576267dfd83715
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-17 11:51:03 -0500

    feat: replace native button with custom Button component in LogoutButton

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import { signOut } from 'next-auth/react';
+import { Button } from "@/components/ui/button"
 
 export function LogoutButton() {
     return (
-        <button type="button" onClick={() => signOut({ redirectTo: '/' })}>
+        <Button variant="default" type="button" onClick={() => signOut({ redirectTo: '/' })}>
             Cerrar Sesión
-        </button>
+        </Button>
     );
 }
