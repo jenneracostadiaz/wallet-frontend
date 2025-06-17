acc8e9c9f997e33de7d87698916bb76e046f6a4f
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-17 09:25:42 -0500

    feat: update type annotations for children prop in layout and providers components

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { SessionProvider } from 'next-auth/react';
+import type { ReactNode } from 'react';
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export function Providers({ children }: { children: ReactNode }) {
     return <SessionProvider>{children}</SessionProvider>;
 }
