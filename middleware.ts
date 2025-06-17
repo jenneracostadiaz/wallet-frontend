98d020bc7655476faab2249a59f6c34f9ff422ff
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-17 10:53:56 -0500

    feat: add middleware for authentication and route protection

---
+++ middleware.ts
@@ -0,0 +1,25 @@
+import { auth } from '@/app/api/auth/[...nextauth]/route';
+import { NextResponse } from 'next/server';
+
+const publicRoutes = ['/login', '/register'];
+
+export default auth(req => {
+    const { pathname } = req.nextUrl;
+
+    const isPublicRoute = publicRoutes.includes(pathname);
+
+    if (!isPublicRoute && !req.auth) {
+        const loginUrl = new URL('/login', req.url);
+        return NextResponse.redirect(loginUrl);
+    }
+
+    if (isPublicRoute && req.auth) {
+        return NextResponse.redirect(new URL('/dashboard', req.url));
+    }
+
+    return NextResponse.next();
+});
+
+export const config = {
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
