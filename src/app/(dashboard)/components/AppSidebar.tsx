d3042a5cfc49bee5f022938d2e47c5998e67439c
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-19 03:21:19 -0500

    feat: enhance AppSidebar with Wallet menu item and update layout structure

--- src/app/(dashboard)/components/AppSidebar.tsx
+++ src/app/(dashboard)/components/AppSidebar.tsx
@@ -1,20 +1,36 @@
+import { Wallet } from "lucide-react"
+
 import {
 	Sidebar,
-	SidebarContent,
-	SidebarFooter,
-	SidebarGroup,
-	SidebarHeader,
+	SidebarContent, SidebarHeader, SidebarFooter,
+	SidebarMenu, SidebarMenuButton, SidebarMenuItem,
 } from "@/components/ui"
+
 
 export function AppSidebar() {
 	return (
-		<Sidebar>
-			<SidebarHeader />
+		<Sidebar variant="inset">
+			<SidebarHeader>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<SidebarMenuButton size="lg" asChild>
+							<a href="#">
+								<div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+									<Wallet className="size-4" />
+								</div>
+								<div className="grid flex-1 text-left text-sm leading-tight">
+									<span className="truncate font-medium">Wallet App</span>
+									<span className="truncate text-xs">Personal Finance</span>
+								</div>
+							</a>
+						</SidebarMenuButton>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarHeader>
 			<SidebarContent>
-				<SidebarGroup />
-				<SidebarGroup />
 			</SidebarContent>
-			<SidebarFooter />
+			<SidebarFooter>
+			</SidebarFooter>
 		</Sidebar>
 	)
 }
--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import {ReactNode} from "react";
-import {SidebarProvider, SidebarTrigger} from "@/components/ui";
+import {SidebarProvider, SidebarTrigger, SidebarInset} from "@/components/ui";
 import {AppSidebar} from "@/app/(dashboard)/components/AppSidebar";
+import {ModeToggle} from "@/components/ModeToggle";
 
 interface DashboardLayoutProps {
 	children: ReactNode;
@@ -10,10 +11,15 @@ export default function DashboardLayout({children}: DashboardLayoutProps) {
 	return (
 		<SidebarProvider>
 			<AppSidebar />
-			<main>
-				<SidebarTrigger />
-				{children}
-			</main>
+			<SidebarInset>
+				<main className="flex flex-1 flex-col gap-4 p-4 pt-2">
+					<div className="flex justify-between items-center">
+						<SidebarTrigger />
+						<ModeToggle />
+					</div>
+					{children}
+				</main>
+			</SidebarInset>
 		</SidebarProvider>
 	);
 }
