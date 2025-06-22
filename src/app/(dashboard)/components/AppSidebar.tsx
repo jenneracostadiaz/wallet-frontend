730904d4dde6b0ee8e7fc6c5b5ddf80473f9445b
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-22 12:18:32 -0500

    fix: update Transactions link in AppSidebar to point to the correct URL

--- src/app/(dashboard)/components/AppSidebar.tsx
+++ src/app/(dashboard)/components/AppSidebar.tsx
@@ -39,7 +39,7 @@ export async function AppSidebar() {
         },
         {
             title: 'Transactions',
-            url: '#',
+            url: '/transactions',
             icon: Coins,
         },
         {
