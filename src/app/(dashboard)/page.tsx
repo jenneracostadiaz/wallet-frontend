4666b75ea25e5f0ca42b586fd30d71452be47821
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-27 08:44:54 -0700

    feat: integrate Header component with breadcrumbs into Home page

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,20 +1,18 @@
-'use client';
+import {Header} from "@/components/Header";
 
-import { useSession } from 'next-auth/react';
+const breadcrumbs = [
+    {
+        title: 'Dashboard',
+        href: '/'
+    }
+];
 
 export default function Home() {
-    const { data: session, status } = useSession();
-
-    if (status === 'loading') {
-        return <p>Loading...</p>;
-    }
-
     return (
         <>
-            <h1>Dashboard</h1>
-            <p>Bienvenido, {session?.user?.name}!</p>
-            <p>Tu email es: {session?.user?.email}</p>
-            <p>Tu token de acceso es: {session?.expires}</p>
+            <Header breadcrumbs={breadcrumbs} />
+            <section className="flex flex-col p-4 pt-0">
+            </section>
         </>
     );
 }
