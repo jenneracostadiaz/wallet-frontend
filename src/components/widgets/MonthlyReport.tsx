a073618fccba17dcd1f1883704669c746def34cb
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-28 12:54:07 -0500

    feat: implement MonthlyReport component with error handling and summary display

--- src/components/widgets/MonthlyReport.tsx
+++ src/components/widgets/MonthlyReport.tsx
@@ -1,8 +1,33 @@
+'use client';
+
+import {useGetMonthlyReport} from "@/hooks/useMonthlyReport";
+import type {monthlyReport} from "@/type/MonthlyReport";
+import {Terminal} from "lucide-react";
+import {Alert, AlertDescription, AlertTitle} from "@/components/ui";
+import {Summary} from "@/components/reports/Summary";
+
 export const MonthlyReport = () => {
+    const { data, isLoading, isError } = useGetMonthlyReport();
+    const monthlyReport: monthlyReport = data?.data;
+    // console.log('Monthly Report Data:', monthlyReport);
+
     return (
-        <section className="flex flex-col p-4 pt-0">
-            <h2 className="text-lg font-semibold">Monthly Report</h2>
-            <p className="text-muted-foreground">This feature is coming soon!</p>
+        <section className="flex flex-col">
+            <h2 className="scroll-m-20 border-b pb-2 text-2xl font-semibold tracking-tight first:mt-0 mb-4">Monthly Report</h2>
+            {isError && (
+                <Alert variant="destructive">
+                    <Terminal />
+                    <AlertTitle>Monthly Report Error</AlertTitle>
+                    <AlertDescription>
+                        Error fetching monthly report. Please try again later.
+                    </AlertDescription>
+                </Alert>
+            )}
+            {!isError && (
+                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    <Summary summary={monthlyReport?.summary} currency={monthlyReport?.currency} loading={isLoading} />
+                </div>
+            )}
         </section>
     );
 };
