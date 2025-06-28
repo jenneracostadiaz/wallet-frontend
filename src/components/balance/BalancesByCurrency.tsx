1c65c54705bec0f9d2f64ae9680989c5a819a399
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-27 21:16:49 -0500

    feat: enhance BalancesByCurrency component with ScrollArea for improved scrolling and loading state display

--- src/components/balance/BalancesByCurrency.tsx
+++ src/components/balance/BalancesByCurrency.tsx
@@ -1,4 +1,4 @@
-import {Card, CardDescription, CardHeader, CardTitle, Skeleton} from "@/components/ui";
+import {Card, CardDescription, CardHeader, CardTitle, ScrollArea, Skeleton} from "@/components/ui";
 import {Account} from "@/type/Accounts";
 
 interface BalancesByCurrencyProps {
@@ -10,20 +10,29 @@ export const BalancesByCurrency = ({accounts, loading}: BalancesByCurrencyProps)
     return (
         <>
             {loading && (
-                <Skeleton className="h-44 w-full rounded-2xl" />
+                <section className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:col-span-2">
+                    <Skeleton className="h-24 w-full rounded-2xl" />
+                    <Skeleton className="h-24 w-full rounded-2xl" />
+                    <Skeleton className="h-24 w-full rounded-2xl" />
+                    <Skeleton className="h-24 w-full rounded-2xl" />
+                    <Skeleton className="h-24 w-full rounded-2xl" />
+                    <Skeleton className="h-24 w-full rounded-2xl" />
+                </section>
             )}
 
             {!loading && (
-                <div className="col-span-2 grid grid-cols-1 gap-4 sm:grid-cols-3">
-                    {accounts?.map((account, index) => (
-                        <Card key={index} className="@container/card">
-                            <CardHeader>
-                                <CardDescription>{account.type} ({account.count})</CardDescription>
-                                <CardTitle>${account.total_balance.toFixed(2)}</CardTitle>
-                            </CardHeader>
-                        </Card>
-                    ))}
-                </div>
+                <ScrollArea className="lg:col-span-2 md:h-52 w-full rounded-2xl">
+                    <section className="grid grid-cols-1 gap-4 lg:grid-cols-3">
+                        {accounts?.map((account, index) => (
+                            <Card key={index} className="@container/card">
+                                <CardHeader>
+                                    <CardDescription>{account.type} ({account.count})</CardDescription>
+                                    <CardTitle>${account.total_balance.toFixed(2)}</CardTitle>
+                                </CardHeader>
+                            </Card>
+                        ))}
+                    </section>
+                </ScrollArea>
             )}
         </>
     )
