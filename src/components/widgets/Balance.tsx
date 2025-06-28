10c3c4db399ee7c28267ff1fdc73ddd3d16efc5c
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-28 10:58:15 -0500

    feat: update Balance component to integrate BalanceByAccounts and adjust data handling for BalancesByCurrency and TotalBalance

--- src/components/widgets/Balance.tsx
+++ src/components/widgets/Balance.tsx
@@ -4,13 +4,13 @@ import {useGetBalance} from "@/hooks/useBalance";
 import {TotalBalance} from "@/components/balance/TotalBalance";
 import {Alert, AlertDescription, AlertTitle} from "@/components/ui";
 import {Terminal} from "lucide-react";
-import {BalancesByCurrency} from "@/components/balance/BalancesByCurrency";
+import type {Balance as BalanceType} from "@/type/Balance";
+import {BalanceByAccounts} from "@/components/balance/BalanceByAccounts";
 
 export const Balance = () => {
 
     const { balance, isLoading, isError } = useGetBalance();
-    const balanceData = balance?.data;
-    console.log('balanceData', balanceData);
+    const balanceData: BalanceType = balance?.data;
 
     return (
         <>
@@ -26,8 +26,9 @@ export const Balance = () => {
 
             {!isError && (
                 <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-                    <TotalBalance loading={isLoading} balance={balanceData?.total_balance} />
-                    <BalancesByCurrency loading={isLoading} accounts={balanceData?.accounts_summary} />
+                    <BalancesByCurrency loading={isLoading} balances={balanceData?.balances_by_currency} />
+                    <TotalBalance loading={isLoading} currency={balanceData?.total_balance.currency} balance={balanceData?.total_balance.total} />
+                    <BalanceByAccounts loading={isLoading} accounts={balanceData?.accounts_summary} />
                 </div>
             )}
         </>
