25df012da284698cb4fc76bd8f09584ec92889bb
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-28 10:55:15 -0500

    feat: add BalanceByAccounts component for displaying account balances with loading state

---
+++ src/components/balance/BalanceByAccounts.tsx
@@ -0,0 +1,39 @@
+import {Card, CardDescription, CardTitle, CardHeader, ScrollArea, Skeleton, CardFooter} from "@/components/ui";
+import type {Account} from "@/type/Accounts";
+
+interface Props {
+    loading: boolean;
+    accounts?: Account[];
+}
+
+export const BalanceByAccounts = ({loading, accounts}) => {
+    return (
+        <>
+            {loading && (
+                <section className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:col-span-2">
+                    <Skeleton className="h-24 w-full rounded-2xl" />
+                    <Skeleton className="h-24 w-full rounded-2xl" />
+                    <Skeleton className="h-24 w-full rounded-2xl" />
+                    <Skeleton className="h-24 w-full rounded-2xl" />
+                    <Skeleton className="h-24 w-full rounded-2xl" />
+                    <Skeleton className="h-24 w-full rounded-2xl" />
+                </section>
+            )}
+
+            {!loading && (
+                <ScrollArea className="lg:col-span-2 md:h-[182px] w-full">
+                    <section className="grid grid-cols-1 gap-4 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6">
+                        {accounts?.map((account: Account, index) => (
+                            <Card key={index} className="@container/card py-2" style={{borderColor: account.color}} >
+                                <CardHeader>
+                                    <CardDescription>{account.name} ({account.type})</CardDescription>
+                                    <CardTitle>{account.currency.symbol}{account.balance}<span className="text-muted-foreground text-xs">{account.currency.code}</span></CardTitle>
+                                </CardHeader>
+                            </Card>
+                        ))}
+                    </section>
+                </ScrollArea>
+            )}
+        </>
+    )
+}
