2af0040544efad780322b21fa4b6d857ab376706
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-28 11:12:12 -0500

    feat: add MonthlyReport component to the dashboard page

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,6 @@
 import {Header} from "@/components/Header";
 import {Balance} from "@/components/widgets";
+import {MonthlyReport} from "@/components/widgets/MonthlyReport";
 
 const breadcrumbs = [
     {
@@ -12,8 +13,9 @@ export default function Home() {
     return (
         <>
             <Header breadcrumbs={breadcrumbs} />
-            <section className="flex flex-col p-4 pt-0">
+            <section className="flex flex-col gap-4 p-4 pt-0">
                 <Balance />
+                <MonthlyReport />
             </section>
         </>
     );
