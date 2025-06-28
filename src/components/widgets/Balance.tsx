bb62296fbf574d91488aa2b4ae967f88b2bb00df
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-27 20:25:39 -0500

    feat: enhance Balance component to handle loading and error states with improved UI

--- src/components/widgets/Balance.tsx
+++ src/components/widgets/Balance.tsx
@@ -5,12 +5,24 @@ import {useGetBalance} from "@/hooks/useBalance";
 export const Balance = () => {
 
     const { balance, isLoading, isError } = useGetBalance();
-    const balanceData = balance.data;
+    const balanceData = balance?.data;
+    console.log('balanceData', balanceData);
+
+    if (isError) {
+        return <div className="text-red-500">Error al cargar el balance</div>;
+    }
 
     return (
-        <div className="p-4 shadow rounded">
-            <h2 className="text-lg font-semibold">Balance: S/{balanceData?.total_balance}</h2>
-        </div>
-    );
+        isLoading ? (
+            <div className="text-gray-500">Cargando...</div>
+        ) : (
+            <div className="flex flex-col gap-4">
+                <h2 className="text-xl font-bold">Balance</h2>
+                <div className="p-4 rounded shadow">
+                    <p className="text-lg font-semibold">Saldo Actual: ${balanceData?.total_balance}</p>
+                </div>
+            </div>
+        )
+    )
 
 }
