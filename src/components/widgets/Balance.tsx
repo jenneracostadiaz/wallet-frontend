0c905c5010bbc163dbaaa2eb026d90e18a1f8a95
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-27 20:10:57 -0500

    feat: update Balance component to fetch balance from new API URL and improve query handling

--- src/components/widgets/Balance.tsx
+++ src/components/widgets/Balance.tsx
@@ -1,9 +1,10 @@
 'use client';
+
+import {useSession} from "next-auth/react";
 import {useQuery} from "@tanstack/react-query";
-import { useSession } from "next-auth/react";
 
 const fetchBalance = async (token: string) => {
-    const response = await fetch(`${process.env.REACT_APP_BALANCE_URL}/dashboard/balance`, {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/dashboard/balance`, {
         headers: {
             Authorization: `Bearer ${token}`,
         },
@@ -16,14 +17,20 @@ const fetchBalance = async (token: string) => {
 
 export const Balance = () => {
     const { data: session } = useSession();
-    const token = session?.accessToken || "";
-    const { data, isLoading, isError } = useQuery({
-        queryKey: ['balance', token],
-        queryFn: () => fetchBalance(token),
-        enabled: !!token,
-    });
 
-    console.log(token, data, isLoading, isError);
+    const {data: balance, isLoading, isError} = useQuery({
+        queryKey: ['balance', session?.accessToken],
+        queryFn: () => fetchBalance(session?.accessToken || ''),
+        enabled: !!session?.accessToken, // Only run the query if we have a token
+        refetchOnWindowFocus: false,
+    })
 
-    return <p>Balance</p>
+    console.log('balance', balance);
+
+    return (
+        <div className="p-4 shadow rounded">
+            <h2 className="text-lg font-semibold">Balance: S/{balance?.data.total_balance}</h2>
+        </div>
+    );
+
 }
