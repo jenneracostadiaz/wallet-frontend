b10d1f6f5e19d7ad582190b65ac1c112ac3ae1ad
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-29 08:28:11 -0500

    feat: enhance AccountsColumns to display account type and description with improved styling

--- src/components/accounts/AccountsColumns.tsx
+++ src/components/accounts/AccountsColumns.tsx
@@ -2,19 +2,27 @@
 import type { Account } from '@/type/Accounts';
 import type { Currency } from '@/type/Currencies';
 import type { ColumnDef } from '@tanstack/table-core';
+import { CircleDashed } from 'lucide-react';
 
 export const AccountsColumns: ColumnDef<Account>[] = [
     {
         accessorKey: 'name',
         header: 'Account Name',
-    },
-    {
-        accessorKey: 'type',
-        header: 'Account Type',
-    },
-    {
-        accessorKey: 'description',
-        header: 'Description',
+        cell: ({ row, getValue }) => {
+            const value = getValue() as string;
+            const color: string = row.original.color;
+            console.log(color);
+            return (
+                <span className="flex flex-col">
+                    <span className="font-semibold capitalize flex items-center gap-1">
+                        <CircleDashed className="size-4" style={{ color: color }} /> {value}
+                    </span>
+                    <span>
+                        <span className="text-xs text-muted-foreground">{row.original.type}</span>
+                    </span>
+                </span>
+            );
+        },
     },
     {
         accessorKey: 'balance',
@@ -27,5 +35,9 @@ export const AccountsColumns: ColumnDef<Account>[] = [
                 currency: currency.code,
             }).format(value);
         },
+    },
+    {
+        accessorKey: 'description',
+        header: 'Description',
     },
 ];
