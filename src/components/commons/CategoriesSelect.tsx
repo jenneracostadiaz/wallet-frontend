ef9dbfce7ba4e7cf673fb720b2d26a2373580436
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-29 15:40:35 -0500

    refactor(categories): update category selection logic and improve placeholder text

--- src/components/commons/CategoriesSelect.tsx
+++ src/components/commons/CategoriesSelect.tsx
@@ -1,7 +1,6 @@
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui';
 import { useGetParentCategories } from '@/hooks/useCategories';
 import type { Category } from '@/type/Categories';
-import { useEffect } from 'react';
 
 interface CategoriesSelectProps {
     value: string;
@@ -12,21 +11,22 @@ interface CategoriesSelectProps {
 export const CategoriesSelect = ({ value, onChange, category }: CategoriesSelectProps) => {
     const { parentCategories, isLoading, isError } = useGetParentCategories({ category });
     const categoryIdExists = parentCategories?.some((c: Category) => String(c.id) === String(value));
-    console.log(parentCategories, categoryIdExists);
-    useEffect(() => {
-        if (!isLoading && !isError && parentCategories.length > 0 && !categoryIdExists) {
-            onChange(String(parentCategories[0].id));
-        }
-    }, [isLoading, isError, parentCategories, categoryIdExists, onChange]);
 
     return (
-        <Select onValueChange={onChange} value={categoryIdExists ? value : ''} disabled={isLoading || isError}>
+        <Select
+            onValueChange={val => onChange(val === 'none' ? '' : val)}
+            value={categoryIdExists ? value : ''}
+            disabled={isLoading || isError}
+        >
             <SelectTrigger className="w-full">
                 <SelectValue
-                    placeholder={isLoading ? 'Loading...' : isError ? 'Error loading categories' : 'Select category'}
+                    placeholder={
+                        isLoading ? 'Loading...' : isError ? 'Error loading categories' : 'Sin categoría padre'
+                    }
                 />
             </SelectTrigger>
             <SelectContent>
+                <SelectItem value="none">Sin categoría padre</SelectItem>
                 {!isLoading &&
                     !isError &&
                     parentCategories?.map((category: Category) => (
