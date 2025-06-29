14c59c2eb589c97109e4e4aa6d0317f5aeef8a3f
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-29 15:08:32 -0500

    feat(categories): add CategoriesSelect component for selecting parent categories

---
+++ src/components/commons/CategoriesSelect.tsx
@@ -0,0 +1,41 @@
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui';
+import { useGetCategories, useGetParentCategories } from '@/hooks/useCategories';
+import type { Category } from '@/type/Categories';
+import { useEffect } from 'react';
+
+interface CategoriesSelectProps {
+    value: string;
+    onChange: (value: string) => void;
+    category: Category;
+}
+
+export const CategoriesSelect = ({ value, onChange, category }: CategoriesSelectProps) => {
+    const { parentCategories, isLoading, isError } = useGetParentCategories({ category });
+    const categoryIdExists = parentCategories?.some((c: Category) => String(c.id) === String(value));
+    console.log(parentCategories, categoryIdExists);
+    useEffect(() => {
+        if (!isLoading && !isError && parentCategories.length > 0 && !categoryIdExists) {
+            onChange(String(parentCategories[0].id));
+        }
+    }, [isLoading, isError, parentCategories, categoryIdExists, onChange]);
+
+    return (
+        <Select onValueChange={onChange} value={categoryIdExists ? value : ''} disabled={isLoading || isError}>
+            <SelectTrigger className="w-full">
+                <SelectValue
+                    placeholder={isLoading ? 'Loading...' : isError ? 'Error loading categories' : 'Select category'}
+                />
+            </SelectTrigger>
+            <SelectContent>
+                {!isLoading &&
+                    !isError &&
+                    parentCategories?.map((category: Category) => (
+                        <SelectItem key={category.id} value={String(category.id)}>
+                            {category.icon} {category.name}
+                            <span className="text-muted-foreground text-xs">(type:{category.type})</span>
+                        </SelectItem>
+                    ))}
+            </SelectContent>
+        </Select>
+    );
+};
