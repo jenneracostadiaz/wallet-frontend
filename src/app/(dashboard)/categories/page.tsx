7e83e40bd1ebabfa7ae7d7c4799edf0a54226594
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-29 13:25:54 -0500

    feat(categories): implement CategoriesPage with loading and error handling

--- src/app/(dashboard)/categories/page.tsx
+++ src/app/(dashboard)/categories/page.tsx
@@ -1,4 +1,12 @@
+'use client';
+import { DataTable } from '@/components/DataTable';
 import { Header } from '@/components/Header';
+import { AccountsColumns } from '@/components/accounts/AccountsColumns';
+import { categoriesColumn } from '@/components/categories/CategoriesColumn';
+import { Alert, AlertDescription, AlertTitle, Skeleton } from '@/components/ui';
+import { useCategoriesTableData, useGetCategories } from '@/hooks/useCategories';
+import type { Category } from '@/type/Categories';
+import { Terminal } from 'lucide-react';
 
 const breadcrumbs = [
     {
@@ -12,6 +20,8 @@ const breadcrumbs = [
 ];
 
 export default function CategoriesPage() {
+    const { data, isLoading, isError } = useGetCategories();
+    const categories: Category[] = useCategoriesTableData({ categories: data?.data });
     return (
         <>
             <Header breadcrumbs={breadcrumbs} />
@@ -19,6 +29,35 @@ export default function CategoriesPage() {
                 <div className="flex justify-between items-center">
                     <h1 className="text-2xl font-bold">Categories</h1>
                 </div>
+
+                {isError && (
+                    <Alert variant="destructive">
+                        <Terminal />
+                        <AlertTitle>Categories Error</AlertTitle>
+                        <AlertDescription>Error fetching categories. Please try again later.</AlertDescription>
+                    </Alert>
+                )}
+
+                {!isError &&
+                    (isLoading ? (
+                        <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4">
+                            <Skeleton className="w-full h-6" />
+                            <Skeleton className="w-full h-6" />
+                            <Skeleton className="w-full h-6" />
+                            <Skeleton className="w-full h-6" />
+                            <Skeleton className="w-full h-6" />
+                            <Skeleton className="w-full h-6" />
+                            <Skeleton className="w-full h-6" />
+                            <Skeleton className="w-full h-6" />
+                            <Skeleton className="w-full h-6" />
+                            <Skeleton className="w-full h-6" />
+                            <Skeleton className="w-full h-6" />
+                            <Skeleton className="w-full h-6" />
+                        </div>
+                    ) : (
+                        categories &&
+                        categories.length > 0 && <DataTable columns={categoriesColumn} data={categories} />
+                    ))}
             </section>
         </>
     );
