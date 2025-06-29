bc9e3c572783aefd4e5072a809bed3b76e2a6600
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-28 19:14:48 -0500

    feat: add useTransactionsTableData hook for transaction data processing

--- src/hooks/useLatestTransactions.ts
+++ src/hooks/useLatestTransactions.ts
@@ -1,5 +1,8 @@
+import type { Account } from '@/type/Accounts';
+import type { Transaction } from '@/type/Transactions';
 import { useQuery } from '@tanstack/react-query';
 import { useSession } from 'next-auth/react';
+import { useMemo } from 'react';
 
 export const fetchLatestTransactions = async (token: string) => {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/dashboard/latest-transactions`, {
@@ -28,4 +31,25 @@ export const useGetLatestTransactions = () => {
         isLoading,
         isError,
     };
+};
+
+interface UseGetLatestTransactionsProps {
+    transactions?: Transaction[];
+}
+
+export const useTransactionsTableData = ({ transactions }: UseGetLatestTransactionsProps) => {
+    return useMemo(() => {
+        if (!transactions) return [];
+        return transactions.map(transaction => {
+            const account: Account = transaction.account;
+            const totalAmount = `
+                ${account.currency} ${transaction.amount}
+            `;
+
+            return {
+                ...transaction,
+                totalAmount,
+            };
+        });
+    }, [transactions]);
 };
