128a0100d3281cc3a82b1a91e44e0e0202310ae0
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-29 08:20:29 -0500

    feat: extend AccountsColumns to include account type, description, and formatted balance

--- src/components/accounts/AccountsColumns.tsx
+++ src/components/accounts/AccountsColumns.tsx
@@ -1,10 +1,31 @@
 'use client';
 import type { Account } from '@/type/Accounts';
+import type { Currency } from '@/type/Currencies';
 import type { ColumnDef } from '@tanstack/table-core';
 
 export const AccountsColumns: ColumnDef<Account>[] = [
     {
         accessorKey: 'name',
         header: 'Account Name',
+    },
+    {
+        accessorKey: 'type',
+        header: 'Account Type',
+    },
+    {
+        accessorKey: 'description',
+        header: 'Description',
+    },
+    {
+        accessorKey: 'balance',
+        header: 'Balance',
+        cell: ({ row, getValue }) => {
+            const value = getValue() as number;
+            const currency: Currency = row.original.currency;
+            return new Intl.NumberFormat('es-PE', {
+                style: 'currency',
+                currency: currency.code,
+            }).format(value);
+        },
     },
 ];
