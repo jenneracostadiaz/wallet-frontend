210d9c8d337adcb415180d9cf61a3094ef21a4b9
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-28 19:14:52 -0500

    feat: update LatestTransactionsColumns to use totalAmount and add visual indicators for income and expenses

--- src/components/transactions/LatestTransactionsColumns.tsx
+++ src/components/transactions/LatestTransactionsColumns.tsx
@@ -1,6 +1,7 @@
 'use client';
 import type { Transaction } from '@/type/Transactions';
 import type { ColumnDef } from '@tanstack/table-core';
+import { TrendingDown, TrendingUp } from 'lucide-react';
 
 export const LatestTransactionsColumns: ColumnDef<Transaction>[] = [
     {
@@ -12,8 +13,22 @@ export const LatestTransactionsColumns: ColumnDef<Transaction>[] = [
         },
     },
     {
-        accessorKey: 'amount',
+        accessorKey: 'totalAmount',
         header: 'Amount',
+        cell: ({ row }) => {
+            const type = row.original.type;
+            const amount = row.getValue('totalAmount');
+            return (
+                <span className="flex items-center gap-2">
+                    {type === 'income' ? (
+                        <TrendingUp color="green" size={16} />
+                    ) : (
+                        <TrendingDown color="red" size={16} />
+                    )}
+                    {String(amount)}
+                </span>
+            );
+        },
     },
     {
         accessorKey: 'description',
