caa4ceea8ba01bc531d7ed901a5875706c6813bc
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-29 16:11:25 -0500

    feat(transactions): implement TransactionsPage with transaction fetching and error handling

--- src/app/(dashboard)/transactions/page.tsx
+++ src/app/(dashboard)/transactions/page.tsx
@@ -1,4 +1,10 @@
+'use client';
+import { DataTable } from '@/components/DataTable';
 import { Header } from '@/components/Header';
+import { TransactionsColum } from '@/components/transactions/TransactionsColum';
+import { Alert, AlertDescription, AlertTitle, Skeleton } from '@/components/ui';
+import { useGetTransactions } from '@/hooks/useTransactions';
+import { Terminal } from 'lucide-react';
 
 const breadcrumbs = [
     {
@@ -12,9 +18,45 @@ const breadcrumbs = [
 ];
 
 export default function TransactionsPage() {
+    const { data, isLoading, isError } = useGetTransactions();
+    const transactions = data?.data || [];
     return (
         <>
             <Header breadcrumbs={breadcrumbs} />
+            <section className="px-4">
+                <div className="flex justify-between items-center mb-4">
+                    <h1 className="text-2xl font-bold">Transactions</h1>
+                </div>
+
+                {isError && (
+                    <Alert variant="destructive">
+                        <Terminal />
+                        <AlertTitle>Transactions Error</AlertTitle>
+                        <AlertDescription>Error fetching transactions. Please try again later.</AlertDescription>
+                    </Alert>
+                )}
+
+                {!isError &&
+                    (isLoading ? (
+                        <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-4">
+                            <Skeleton className="w-full h-6" />
+                            <Skeleton className="w-full h-6" />
+                            <Skeleton className="w-full h-6" />
+                            <Skeleton className="w-full h-6" />
+                            <Skeleton className="w-full h-6" />
+                            <Skeleton className="w-full h-6" />
+                            <Skeleton className="w-full h-6" />
+                            <Skeleton className="w-full h-6" />
+                            <Skeleton className="w-full h-6" />
+                            <Skeleton className="w-full h-6" />
+                            <Skeleton className="w-full h-6" />
+                            <Skeleton className="w-full h-6" />
+                        </div>
+                    ) : (
+                        transactions &&
+                        transactions.length > 0 && <DataTable columns={TransactionsColum} data={transactions} />
+                    ))}
+            </section>
         </>
     );
 }
