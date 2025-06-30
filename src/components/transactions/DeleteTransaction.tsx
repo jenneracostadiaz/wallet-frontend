886cca4c563414601313cf48d69963c40f34d170
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-29 20:06:21 -0500

    feat(transactions): add DeleteTransaction component for transaction deletion

---
+++ src/components/transactions/DeleteTransaction.tsx
@@ -0,0 +1,45 @@
+'use client';
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+    DropdownMenuItem,
+} from '@/components/ui';
+import { useTransactionDelete } from '@/hooks/useTransactions';
+import type { Transaction } from '@/type/Transactions';
+import { useState } from 'react';
+
+export const DeleteTransaction = ({ transaction }: { transaction: Transaction }) => {
+    const [open, setOpen] = useState(false);
+    const { mutate, isPending } = useTransactionDelete({ transaction, setOpen });
+    return (
+        <AlertDialog open={open} onOpenChange={setOpen}>
+            <AlertDialogTrigger asChild>
+                <DropdownMenuItem variant="destructive" onSelect={e => e.preventDefault()}>
+                    Delete
+                </DropdownMenuItem>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        This action cannot be undone. This will permanently delete the account and remove its data from
+                        our servers.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction onClick={() => mutate()} disabled={isPending}>
+                        {isPending ? 'Deleting...' : 'Delete'}
+                    </AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    );
+};
