e6119bb085ab756f83dcaa794f964e7443c395c5
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-29 20:20:31 -0500

    feat(transactions): add currency field to Transaction type

--- src/type/Transactions.ts
+++ src/type/Transactions.ts
@@ -1,11 +1,13 @@
 import type { Account } from '@/type/Accounts';
 import type { Category } from '@/type/Categories';
+import type { Currency } from '@/type/Currencies';
 
 export type Transaction = {
     id: string;
     amount: number;
     account: Account;
     category: Category;
+    currency: Currency;
     date: Date;
     date_human?: string;
     description?: string;
