888a02f1aa0be8a5bb164c591b3ffbfde1324182
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-30 12:43:31 -0500

    feat(transactionsPage): add filtering functionality for transaction descriptions

--- src/app/(dashboard)/transactions/page.tsx
+++ src/app/(dashboard)/transactions/page.tsx
@@ -2,9 +2,11 @@
 import { DataTable } from '@/components/DataTable';
 import { Header } from '@/components/Header';
 import { TransactionsColum } from '@/components/transactions/TransactionsColum';
-import { Alert, AlertDescription, AlertTitle, Skeleton } from '@/components/ui';
+import { Alert, AlertDescription, AlertTitle, Input, Skeleton } from '@/components/ui';
 import { useGetTransactions } from '@/hooks/useTransactions';
+import type { ColumnFiltersState } from '@tanstack/react-table';
 import { Terminal } from 'lucide-react';
+import { useState } from 'react';
 
 const breadcrumbs = [
     {
@@ -20,12 +22,32 @@ const breadcrumbs = [
 export default function TransactionsPage() {
     const { data, isLoading, isError } = useGetTransactions();
     const transactions = data?.data || [];
+    const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+
+    const onFilterChange = (id: string, value: string) => {
+        setColumnFilters(prev => {
+            const newFilters = prev.filter(f => f.id !== id);
+            if (value) {
+                newFilters.push({ id, value });
+            }
+            return newFilters;
+        });
+    };
+
     return (
         <>
             <Header breadcrumbs={breadcrumbs} />
             <section className="px-4">
                 <div className="flex justify-between items-center mb-4">
                     <h1 className="text-2xl font-bold">Transactions</h1>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5  gap-4 items-center py-4">
+                    <Input
+                        placeholder="Filter descriptions..."
+                        value={(columnFilters.find(f => f.id === 'description')?.value as string) ?? ''}
+                        onChange={event => onFilterChange('description', event.target.value)}
+                        className="w-full lg:col-span-2"
+                    />
                 </div>
 
                 {isError && (
@@ -55,7 +77,13 @@ export default function TransactionsPage() {
                     ) : (
                         transactions &&
                         transactions.length > 0 && (
-                            <DataTable columns={TransactionsColum} data={transactions} pageSize={20} />
+                            <DataTable
+                                columns={TransactionsColum}
+                                data={transactions}
+                                pageSize={20}
+                                columnFilters={columnFilters}
+                                onColumnFiltersChange={setColumnFilters}
+                            />
                         )
                     ))}
             </section>
