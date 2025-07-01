e11d7282820e7752d099962f553e88e994e57789
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-30 20:01:01 -0500

    feat(transactions): add CreateTransaction component for creating new transactions

---
+++ src/components/transactions/CreateTransaction.tsx
@@ -0,0 +1,21 @@
+'use client';
+import { FormTransaction } from '@/components/transactions/FormTransaction';
+import { Button, Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui';
+import { useState } from 'react';
+
+export const CreateTransaction = () => {
+    const [isCreateModalOpen, setCreateModalOpen] = useState(false);
+    return (
+        <Dialog open={isCreateModalOpen} onOpenChange={setCreateModalOpen}>
+            <DialogTrigger asChild>
+                <Button variant="outline">Create Transaction</Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Add New Transaction</DialogTitle>
+                    <FormTransaction onSuccess={() => setCreateModalOpen(false)} />
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    );
+};
