11a98fd0f01c8f9789a55b6b3915e99c918bd190
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-30 20:34:08 -0500

    feat(transactions): add RadioGroup and RadioGroupItem components for improved radio selection

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -13,6 +13,7 @@ export * from './dropdown-menu';
 export * from './input';
 export * from './label';
 export * from './popover';
+export * from './radio-group';
 export * from './scroll-area';
 export * from './select';
 export * from './separator';
