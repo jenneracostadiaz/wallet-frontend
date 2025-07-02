11e91c7cca652c7a34c6c3304d938e03e3ca0bcc
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-07-01 22:49:06 -0500

    feat(transactions): update layout for improved spacing and alignment

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     return (
         <>
             <Header breadcrumbs={breadcrumbs} />
-            <section className="flex flex-col gap-4 p-4 pt-0">
+            <section className="flex flex-col gap-12 p-4 pt-0 w-full max-w-7xl mx-auto">
                 <Balance />
                 <MonthlyReport />
                 <LatestTransactions />
