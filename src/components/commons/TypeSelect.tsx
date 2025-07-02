f1980c0a91e7c912450125482df4c1fcb7e0535d
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-07-01 23:24:21 -0500

    feat(type-select): add expense type with corresponding icon to type options

--- src/components/commons/TypeSelect.tsx
+++ src/components/commons/TypeSelect.tsx
@@ -1,14 +1,22 @@
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui';
-import {CircleDashed, TrendingDown, TrendingUp} from "lucide-react";
+import { CircleDashed, TrendingDown, TrendingUp } from 'lucide-react';
 
 const types = [
     {
         value: 'income',
         label: 'Income',
-        icon: <TrendingUp className="text-green-400" />
+        icon: <TrendingUp className="text-green-400" />,
     },
-    { value: 'expense', label: 'Expense', icon: <TrendingDown className="text-red-400" /> },
-    { value: 'transfer', label: 'Transfer', icon: <CircleDashed className="text-blue-400" /> },
+    {
+        value: 'expense',
+        label: 'Expense',
+        icon: <TrendingDown className="text-red-400" />,
+    },
+    {
+        value: 'transfer',
+        label: 'Transfer',
+        icon: <CircleDashed className="text-blue-400" />,
+    },
 ];
 
 export const TypeSelect = ({ value, onChange }: { value: string; onChange: (value: string) => void }) => {
