f09be8ec3e5e044baf5e785b4a9b902607afa34d
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-07-01 23:14:02 -0500

    feat(categories-select): enhance category selection with subcategories and improved layout

--- src/components/commons/CategoriesSelect.tsx
+++ src/components/commons/CategoriesSelect.tsx
@@ -1,4 +1,12 @@
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui';
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui';
 import { useGetParentCategories } from '@/hooks/useCategories';
 import type { Category } from '@/type/Categories';
 
@@ -11,7 +19,10 @@ interface CategoriesSelectProps {
 
 export const CategoriesSelect = ({ value, onChange, category, transactionType }: CategoriesSelectProps) => {
     const { parentCategories, isLoading, isError } = useGetParentCategories({ category, transactionType });
-    const categoryIdExists = parentCategories?.some((c: Category) => String(c.id) === String(value));
+    const categoryIdExists = parentCategories?.some(
+        (c: Category) =>
+            String(c.id) === String(value) || c.subcategories?.some((sc: Category) => String(sc.id) === String(value))
+    );
 
     return (
         <Select
@@ -28,12 +39,32 @@ export const CategoriesSelect = ({ value, onChange, category, transactionType }:
                 <SelectItem value="none">No category</SelectItem>
                 {!isLoading &&
                     !isError &&
-                    parentCategories?.map((category: Category) => (
-                        <SelectItem key={category.id} value={String(category.id)}>
-                            {category.icon} {category.name}
-                            <span className="text-muted-foreground text-xs">(type:{category.type})</span>
-                        </SelectItem>
-                    ))}
+                    parentCategories?.map((category: Category) =>
+                        category.subcategories && category.subcategories.length > 0 ? (
+                            <SelectGroup key={category.id}>
+                                <SelectLabel className="capitalize">
+                                    {category.icon} {category.name}
+                                </SelectLabel>
+                                {category.subcategories?.map((subcategory: Category) => (
+                                    <SelectItem
+                                        key={subcategory.id}
+                                        value={String(subcategory.id)}
+                                        className="flex items-center gap-2 capitalize"
+                                    >
+                                        {subcategory.icon} {subcategory.name}
+                                    </SelectItem>
+                                ))}
+                            </SelectGroup>
+                        ) : (
+                            <SelectItem
+                                key={category.id}
+                                value={String(category.id)}
+                                className="flex items-center gap-2 capitalize"
+                            >
+                                {category.icon} {category.name}
+                            </SelectItem>
+                        )
+                    )}
             </SelectContent>
         </Select>
     );
