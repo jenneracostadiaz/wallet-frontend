47e3617e1818f341b300de85851fa6dc9b91ea48
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-07-01 22:53:47 -0500

    feat(type-select): add icons to type options for better visual distinction

--- src/components/commons/TypeSelect.tsx
+++ src/components/commons/TypeSelect.tsx
@@ -1,9 +1,14 @@
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui';
+import {CircleDashed, TrendingDown, TrendingUp} from "lucide-react";
 
 const types = [
-    { value: 'expense', label: 'Expense' },
-    { value: 'income', label: 'Income' },
-    { value: 'transfer', label: 'Transfer' },
+    {
+        value: 'income',
+        label: 'Income',
+        icon: <TrendingUp className="text-green-400" />
+    },
+    { value: 'expense', label: 'Expense', icon: <TrendingDown className="text-red-400" /> },
+    { value: 'transfer', label: 'Transfer', icon: <CircleDashed className="text-blue-400" /> },
 ];
 
 export const TypeSelect = ({ value, onChange }: { value: string; onChange: (value: string) => void }) => {
@@ -23,7 +28,7 @@ export const TypeSelect = ({ value, onChange }: { value: string; onChange: (valu
                 <SelectItem value="all">All types</SelectItem>
                 {types.map(type => (
                     <SelectItem key={type.value} value={type.value}>
-                        {type.label}
+                        {type.label} {type.icon}
                     </SelectItem>
                 ))}
             </SelectContent>
