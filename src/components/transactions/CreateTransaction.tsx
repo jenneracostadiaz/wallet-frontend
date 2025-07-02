30ab614fb3b48ebe7e592ff2b2262fc516e9c6a3
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-07-02 12:01:07 -0500

    refactor(CreateTransaction): unify modal and drawer components for consistent transaction creation experience

--- src/components/transactions/CreateTransaction.tsx
+++ src/components/transactions/CreateTransaction.tsx
@@ -1,55 +1,22 @@
 'use client';
 import { FormTransaction } from '@/components/transactions/FormTransaction';
-import {
-    Button,
-    Dialog,
-    DialogContent,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-    Drawer,
-    DrawerContent,
-    DrawerFooter,
-    DrawerHeader,
-    DrawerTitle,
-    DrawerTrigger,
-} from '@/components/ui';
-import { useIsMobile } from '@/hooks/use-mobile';
+import { Button, Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui';
 import { useState } from 'react';
 
 export const CreateTransaction = () => {
     const [isCreateModalOpen, setCreateModalOpen] = useState(false);
-    const isMobile = useIsMobile();
-
-    if (!isMobile) {
-        return (
-            <Dialog open={isCreateModalOpen} onOpenChange={setCreateModalOpen}>
-                <DialogTrigger asChild>
-                    <Button variant="outline">Create Transaction</Button>
-                </DialogTrigger>
-                <DialogContent>
-                    <DialogHeader>
-                        <DialogTitle>Add New Transaction</DialogTitle>
-                        <FormTransaction onSuccess={() => setCreateModalOpen(false)} />
-                    </DialogHeader>
-                </DialogContent>
-            </Dialog>
-        );
-    }
 
     return (
-        <Drawer>
-            <DrawerTrigger>
+        <Dialog open={isCreateModalOpen} onOpenChange={setCreateModalOpen}>
+            <DialogTrigger asChild>
                 <Button variant="outline">Create Transaction</Button>
-            </DrawerTrigger>
-            <DrawerContent>
-                <DrawerHeader>
-                    <DrawerTitle>Add New Transaction</DrawerTitle>
-                </DrawerHeader>
-                <DrawerFooter>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Add New Transaction</DialogTitle>
                     <FormTransaction onSuccess={() => setCreateModalOpen(false)} />
-                </DrawerFooter>
-            </DrawerContent>
-        </Drawer>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
     );
 };
