ecdfb94f975388ce259db90fcff16f04799bcfe6
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-07-01 22:49:29 -0500

    feat(balances): update layout for improved column span in balance display

--- src/components/widgets/Balance.tsx
+++ src/components/widgets/Balance.tsx
@@ -23,7 +23,7 @@ export const Balance = () => {
             )}
 
             {!isError && (
-                <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+                <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-4">
                     <BalancesByCurrency loading={isLoading} balances={balanceData?.balances_by_currency} />
                     <TotalBalance
                         loading={isLoading}
