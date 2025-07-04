39d90b8ac8130020cbab5b2a733b8f3f25448c4e
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-07-04 05:36:15 -0500

    refactor(balance): add BalanceSkeleton component for loading state

---
+++ src/components/balance/BalanceSkeleton.tsx
@@ -0,0 +1,23 @@
+import { Skeleton } from '@/components/ui/skeleton';
+
+export const BalanceSkeleton = () => {
+    return (
+        <section className="grid md:grid-cols-2 lg:grid-cols-4 gap-4">
+            <section className="lg:col-span-4 flex w-max space-x-4">
+                <Skeleton className="h-5 w-32 rounded-md" />
+                <Skeleton className="h-5 w-32 rounded-md" />
+                <Skeleton className="h-5 w-32 rounded-md" />
+            </section>
+            <Skeleton className="h-52 w-full rounded-2xl" />
+            <section className="lg:col-span-3 grid grid-cols-1 gap-4 lg:grid-cols-3 xl:grid-cols-4">
+                <Skeleton className="h-24 w-full rounded-2xl" />
+                <Skeleton className="h-24 w-full rounded-2xl" />
+                <Skeleton className="h-24 w-full rounded-2xl" />
+                <Skeleton className="h-24 w-full rounded-2xl" />
+                <Skeleton className="h-24 w-full rounded-2xl" />
+                <Skeleton className="h-24 w-full rounded-2xl" />
+                <Skeleton className="h-24 w-full rounded-2xl" />
+            </section>
+        </section>
+    );
+};
