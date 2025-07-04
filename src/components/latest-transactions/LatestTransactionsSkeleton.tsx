271f10f9e7c08cd29e72dbd6c19bb2a3b2c7be5c
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-07-04 06:08:30 -0500

    refactor(latest-transaction): enhance responsive visibility of skeleton elements

--- src/components/latest-transactions/LatestTransactionsSkeleton.tsx
+++ src/components/latest-transactions/LatestTransactionsSkeleton.tsx
@@ -2,31 +2,31 @@ import { Skeleton } from '@/components/ui';
 
 export const LatestTransactionsSkeleton = () => {
     return (
-        <div className="grid grid-cols-6 gap-4">
+        <div className="grid lg:grid-cols-6 gap-4">
             <Skeleton className="w-full h-12 mx-auto" />
             <Skeleton className="w-full h-12 mx-auto" />
             <Skeleton className="w-full h-12 mx-auto" />
             <Skeleton className="w-full h-12 mx-auto" />
             <Skeleton className="w-full h-12 mx-auto" />
             <Skeleton className="w-full h-12 mx-auto" />
-            <Skeleton className="w-full h-8 mx-auto" />
-            <Skeleton className="w-full h-8 mx-auto" />
-            <Skeleton className="w-full h-8 mx-auto" />
-            <Skeleton className="w-full h-8 mx-auto" />
-            <Skeleton className="w-full h-8 mx-auto" />
-            <Skeleton className="w-full h-8 mx-auto" />
-            <Skeleton className="w-full h-8 mx-auto" />
-            <Skeleton className="w-full h-8 mx-auto" />
-            <Skeleton className="w-full h-8 mx-auto" />
-            <Skeleton className="w-full h-8 mx-auto" />
-            <Skeleton className="w-full h-8 mx-auto" />
-            <Skeleton className="w-full h-8 mx-auto" />
-            <Skeleton className="w-full h-8 mx-auto" />
-            <Skeleton className="w-full h-8 mx-auto" />
-            <Skeleton className="w-full h-8 mx-auto" />
-            <Skeleton className="w-full h-8 mx-auto" />
-            <Skeleton className="w-full h-8 mx-auto" />
-            <Skeleton className="w-full h-8 mx-auto" />
+            <Skeleton className="w-full h-8 mx-auto hidden lg:block" />
+            <Skeleton className="w-full h-8 mx-auto hidden lg:block" />
+            <Skeleton className="w-full h-8 mx-auto hidden lg:block" />
+            <Skeleton className="w-full h-8 mx-auto hidden lg:block" />
+            <Skeleton className="w-full h-8 mx-auto hidden lg:block" />
+            <Skeleton className="w-full h-8 mx-auto hidden lg:block" />
+            <Skeleton className="w-full h-8 mx-auto hidden lg:block" />
+            <Skeleton className="w-full h-8 mx-auto hidden lg:block" />
+            <Skeleton className="w-full h-8 mx-auto hidden lg:block" />
+            <Skeleton className="w-full h-8 mx-auto hidden lg:block" />
+            <Skeleton className="w-full h-8 mx-auto hidden lg:block" />
+            <Skeleton className="w-full h-8 mx-auto hidden lg:block" />
+            <Skeleton className="w-full h-8 mx-auto hidden lg:block" />
+            <Skeleton className="w-full h-8 mx-auto hidden lg:block" />
+            <Skeleton className="w-full h-8 mx-auto hidden lg:block" />
+            <Skeleton className="w-full h-8 mx-auto hidden lg:block" />
+            <Skeleton className="w-full h-8 mx-auto hidden lg:block" />
+            <Skeleton className="w-full h-8 mx-auto hidden lg:block" />
         </div>
     );
 };
