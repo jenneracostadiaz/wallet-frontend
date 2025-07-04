6d5e932aff82485ff16ade66d5493759a4cc6464
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-07-04 05:15:21 -0500

    refactor(latest-transactions): rename prop for consistency and clarity

--- src/components/widgets/LatestTransactions.tsx
+++ src/components/widgets/LatestTransactions.tsx
@@ -3,6 +3,6 @@ import { DataTable } from '@/components/DataTable';
 import { TransactionsColum } from '@/components/transactions/TransactionsColum';
 import type { Transaction } from '@/type/Transactions';
 
-export const LatestTransactions = ({ initialtransactions }: { initialtransactions: Transaction[] }) => {
-    return <DataTable columns={TransactionsColum} data={initialtransactions} pageSize={10} />;
+export const LatestTransactions = ({ initialTransactions }: { initialTransactions: Transaction[] }) => {
+    return <DataTable columns={TransactionsColum} data={initialTransactions} pageSize={10} />;
 };
