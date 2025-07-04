24d55032cb1a9718dae0f5f1ba2d6c3d9249f530
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-07-04 04:27:25 -0500

    refactor(balances): remove loading state and simplify BalancesByCurrency component

--- src/components/balance/BalancesByCurrency.tsx
+++ src/components/balance/BalancesByCurrency.tsx
@@ -1,42 +1,23 @@
-import { Badge, ScrollArea, ScrollBar, Skeleton } from '@/components/ui';
+import { Badge, ScrollArea, ScrollBar } from '@/components/ui';
 import type { BalanceByCurrency } from '@/type/Balance';
 
 interface BalancesByCurrencyProps {
-    balances: BalanceByCurrency[];
-    loading: boolean;
+    balances?: BalanceByCurrency[];
 }
 
-export const BalancesByCurrency = ({ balances, loading }: BalancesByCurrencyProps) => {
+export const BalancesByCurrency = ({ balances }: BalancesByCurrencyProps) => {
     return (
-        <>
-            {loading && (
-                <ScrollArea className="lg:col-span-4 w-[328px] lg:w-96 pb-3 whitespace-nowrap">
-                    <section className="flex w-max space-x-4">
-                        <Skeleton className="h-5 w-32 rounded-md" />
-                        <Skeleton className="h-5 w-32 rounded-md" />
-                        <Skeleton className="h-5 w-32 rounded-md" />
-                        <Skeleton className="h-5 w-32 rounded-md" />
-                        <Skeleton className="h-5 w-32 rounded-md" />
-                        <Skeleton className="h-5 w-32 rounded-md" />
-                    </section>
-                    <ScrollBar orientation="horizontal" />
-                </ScrollArea>
-            )}
-
-            {!loading && (
-                <ScrollArea className="lg:col-span-4 w-[328px] md:w-full pb-3 md:pb-0 whitespace-nowrap">
-                    <section className="flex w-max space-x-4">
-                        {balances?.map((balance: BalanceByCurrency) => (
-                            <Badge key={balance.currency.code} variant="secondary">
-                                {balance.currency.symbol}
-                                {balance.total}
-                                <span className="text-muted-foreground text-xs">{balance.currency.code}</span>
-                            </Badge>
-                        ))}
-                    </section>
-                    <ScrollBar orientation="horizontal" />
-                </ScrollArea>
-            )}
-        </>
+        <ScrollArea className="lg:col-span-4 w-[328px] md:w-full pb-3 md:pb-0 whitespace-nowrap">
+            <section className="flex w-max space-x-4">
+                {balances?.map((balance: BalanceByCurrency) => (
+                    <Badge key={balance.currency.code} variant="secondary">
+                        {balance.currency.symbol}
+                        {balance.total}
+                        <span className="text-muted-foreground text-xs">{balance.currency.code}</span>
+                    </Badge>
+                ))}
+            </section>
+            <ScrollBar orientation="horizontal" />
+        </ScrollArea>
     );
 };
