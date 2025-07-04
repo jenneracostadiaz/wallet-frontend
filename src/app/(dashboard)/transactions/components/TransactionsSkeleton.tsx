0d470b4513b4ba5c63fba81a3b77369fa45c76b8
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-07-04 06:08:48 -0500

    refactor(transactions-skeleton): improve layout and integrate Balance and LatestTransactions skeletons

--- src/app/(dashboard)/transactions/components/TransactionsSkeleton.tsx
+++ src/app/(dashboard)/transactions/components/TransactionsSkeleton.tsx
@@ -1,25 +1,27 @@
-import { Skeleton } from '@/components/ui/skeleton';
+import { BalanceSkeleton } from '@/components/balance';
+import { LatestTransactionsSkeleton } from '@/components/latest-transactions/LatestTransactionsSkeleton';
+import { Skeleton } from '@/components/ui';
 
 export function TransactionsSkeleton() {
     return (
-        <>
-            <div className="hidden md:flex items-center gap-2 p-4 border rounded-lg">
-                <Skeleton className="h-10 flex-1" />
-                <Skeleton className="h-10 flex-1" />
-                <Skeleton className="h-10 flex-1" />
-                <Skeleton className="h-10 w-24" />
-            </div>
-
-            <div className="border rounded-lg p-4">
-                <div className="space-y-2">
-                    <Skeleton className="h-12 w-full" />
-                    <Skeleton className="h-12 w-full" />
-                    <Skeleton className="h-12 w-full" />
-                    <Skeleton className="h-12 w-full" />
-                    <Skeleton className="h-12 w-full" />
-                    <Skeleton className="h-12 w-full" />
+        <div className="grid gap-12 w-11/12 max-w-7xl mx-auto">
+            <BalanceSkeleton />
+            <div className="grid gap-4">
+                <div className="flex flex-wrap justify-between items-center gap-4">
+                    <Skeleton className="w-48 h-10" />
+                    <Skeleton className="w-48 h-10 hidden lg:block" />
                 </div>
+                <div className="hidden lg:grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 items-center">
+                    <Skeleton className="w-full h-8" />
+                    <Skeleton className="w-full h-8" />
+                    <Skeleton className="w-full h-8" />
+                    <Skeleton className="w-full h-8" />
+                    <Skeleton className="w-full h-8" />
+                    <Skeleton className="w-full h-8" />
+                    <Skeleton className="w-full h-8 lg:col-span-2" />
+                </div>
+                <LatestTransactionsSkeleton />
             </div>
-        </>
+        </div>
     );
 }
