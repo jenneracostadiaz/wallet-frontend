f8515594c8cf5897b8292dd7a71bef136fe5a00d
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-07-04 05:53:03 -0500

    feat(dashboard): add LatestTransactionsSkeleton component for loading state

---
+++ src/components/latest-transactions/LatestTransactionsSkeleton.tsx
@@ -0,0 +1,32 @@
+import { Skeleton } from '@/components/ui';
+
+export const LatestTransactionsSkeleton = () => {
+    return (
+        <div className="grid grid-cols-6 gap-4">
+            <Skeleton className="w-full h-12 mx-auto" />
+            <Skeleton className="w-full h-12 mx-auto" />
+            <Skeleton className="w-full h-12 mx-auto" />
+            <Skeleton className="w-full h-12 mx-auto" />
+            <Skeleton className="w-full h-12 mx-auto" />
+            <Skeleton className="w-full h-12 mx-auto" />
+            <Skeleton className="w-full h-8 mx-auto" />
+            <Skeleton className="w-full h-8 mx-auto" />
+            <Skeleton className="w-full h-8 mx-auto" />
+            <Skeleton className="w-full h-8 mx-auto" />
+            <Skeleton className="w-full h-8 mx-auto" />
+            <Skeleton className="w-full h-8 mx-auto" />
+            <Skeleton className="w-full h-8 mx-auto" />
+            <Skeleton className="w-full h-8 mx-auto" />
+            <Skeleton className="w-full h-8 mx-auto" />
+            <Skeleton className="w-full h-8 mx-auto" />
+            <Skeleton className="w-full h-8 mx-auto" />
+            <Skeleton className="w-full h-8 mx-auto" />
+            <Skeleton className="w-full h-8 mx-auto" />
+            <Skeleton className="w-full h-8 mx-auto" />
+            <Skeleton className="w-full h-8 mx-auto" />
+            <Skeleton className="w-full h-8 mx-auto" />
+            <Skeleton className="w-full h-8 mx-auto" />
+            <Skeleton className="w-full h-8 mx-auto" />
+        </div>
+    );
+};
