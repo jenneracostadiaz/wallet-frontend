817f67c3d4b2d571d5360e9ac0bf0621b59cc439
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-07-04 04:28:58 -0500

    refactor(balance): remove loading state and simplify TotalBalance component

--- src/components/balance/TotalBalance.tsx
+++ src/components/balance/TotalBalance.tsx
@@ -1,6 +1,7 @@
-import { Card, CardDescription, CardFooter, CardHeader, CardTitle, Skeleton } from '@/components/ui';
-import type { Currency } from '@/type/Currencies';
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui';
 import { TrendingUp } from 'lucide-react';
+
+import type { Currency } from '@/type/Currencies';
 
 interface TotalBalanceProps {
     currency: Currency;
@@ -8,31 +9,25 @@ interface TotalBalanceProps {
     loading?: boolean;
 }
 
-export const TotalBalance = ({ currency, balance, loading }: TotalBalanceProps) => {
+export const TotalBalance = ({ currency, balance }: TotalBalanceProps) => {
     return (
-        <>
-            {loading && <Skeleton className="h-52 w-full rounded-2xl" />}
-
-            {!loading && (
-                <Card className="@container/card">
-                    <CardHeader>
-                        <CardDescription>Total Balance</CardDescription>
-                        <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-                            {currency?.symbol}
-                            {balance}
-                            <span className="text-muted-foreground text-sm">{currency?.code}</span>
-                        </CardTitle>
-                    </CardHeader>
-                    <CardFooter className="flex-col items-start gap-1.5 text-sm">
-                        <p className="line-clamp-1 flex gap-2 font-medium">
-                            Your balance so far <TrendingUp className="size-4" />
-                        </p>
-                        <p className="text-muted-foreground">
-                            This is the sum of all your main currencies across all accounts.
-                        </p>
-                    </CardFooter>
-                </Card>
-            )}
-        </>
+        <Card className="@container/card">
+            <CardHeader>
+                <CardDescription>Total Balance</CardDescription>
+                <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
+                    {currency?.symbol}
+                    {balance}
+                    <span className="text-muted-foreground text-sm">{currency?.code}</span>
+                </CardTitle>
+            </CardHeader>
+            <CardFooter className="flex-col items-start gap-1.5 text-sm">
+                <p className="line-clamp-1 flex gap-2 font-medium">
+                    Your balance so far <TrendingUp className="size-4" />
+                </p>
+                <p className="text-muted-foreground">
+                    This is the sum of all your main currencies across all accounts.
+                </p>
+            </CardFooter>
+        </Card>
     );
 };
