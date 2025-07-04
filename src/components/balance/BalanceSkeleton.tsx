b5e54198be35dea407eb1e6765703e854a6d2f23
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-07-04 06:06:26 -0500

    refactor(balance): optimize skeleton visibility for responsive design

--- src/components/balance/BalanceSkeleton.tsx
+++ src/components/balance/BalanceSkeleton.tsx
@@ -6,17 +6,16 @@ export const BalanceSkeleton = () => {
             <section className="lg:col-span-4 flex w-max space-x-4">
                 <Skeleton className="h-5 w-32 rounded-md" />
                 <Skeleton className="h-5 w-32 rounded-md" />
-                <Skeleton className="h-5 w-32 rounded-md" />
             </section>
             <Skeleton className="h-52 w-full rounded-2xl" />
             <section className="lg:col-span-3 grid grid-cols-1 gap-4 lg:grid-cols-3 xl:grid-cols-4">
                 <Skeleton className="h-24 w-full rounded-2xl" />
                 <Skeleton className="h-24 w-full rounded-2xl" />
-                <Skeleton className="h-24 w-full rounded-2xl" />
-                <Skeleton className="h-24 w-full rounded-2xl" />
-                <Skeleton className="h-24 w-full rounded-2xl" />
-                <Skeleton className="h-24 w-full rounded-2xl" />
-                <Skeleton className="h-24 w-full rounded-2xl" />
+                <Skeleton className="h-24 w-full rounded-2xl hidden lg:block" />
+                <Skeleton className="h-24 w-full rounded-2xl hidden lg:block" />
+                <Skeleton className="h-24 w-full rounded-2xl hidden lg:block" />
+                <Skeleton className="h-24 w-full rounded-2xl hidden lg:block" />
+                <Skeleton className="h-24 w-full rounded-2xl hidden lg:block" />
             </section>
         </section>
     );
