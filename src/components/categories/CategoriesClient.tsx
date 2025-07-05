38fd546cc2979cb7f9a02c9e6970147351ea6ace
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-07-05 17:11:49 -0500

    feat(categories): add CategoriesClient component for managing category data

---
+++ src/components/categories/CategoriesClient.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { DataTable } from '@/components/DataTable';
+import { categoriesColumn } from '@/components/categories/CategoriesColumn';
+import { CreateCategory } from '@/components/categories/CreateCategory';
+import { useCategoriesFilters } from '@/hooks/useCategoriesFilters';
+
+import { useCategoriesData } from '@/hooks/useCategories';
+import type { Category } from '@/type/Categories';
+
+export function CategoriesClient({ initialCategories }: { initialCategories: { data: Category[] } }) {
+    const subcategories: Category[] = useCategoriesData({ initialCategories });
+
+    const { columnFilters, setColumnFilters } = useCategoriesFilters();
+
+    return (
+        <section className="px-4 w-full max-w-7xl mx-auto">
+            <div className="flex justify-between items-center mb-4">
+                <h1 className="text-2xl font-bold">Categories</h1>
+                <CreateCategory />
+            </div>
+
+            <DataTable
+                columns={categoriesColumn}
+                data={subcategories}
+                pageSize={50}
+                columnFilters={columnFilters}
+                onColumnFiltersChange={setColumnFilters}
+            />
+        </section>
+    );
+}
