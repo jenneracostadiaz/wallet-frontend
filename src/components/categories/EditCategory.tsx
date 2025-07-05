4d48a63ba1668b5233d123ed4a1e99c4aafde72c
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-07-05 17:22:32 -0500

    feat(categories): update EditCategory to accept initialCategories prop for FormCategory

--- src/components/categories/EditCategory.tsx
+++ src/components/categories/EditCategory.tsx
@@ -4,7 +4,12 @@ import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger, Dropdo
 import type { Category } from '@/type/Categories';
 import { useState } from 'react';
 
-export const EditCategory = ({ category }: { category: Category }) => {
+interface EditCategoryProps {
+    category: Category;
+    initialCategories: { data: Category[] };
+}
+
+export const EditCategory = ({ category, initialCategories }: EditCategoryProps) => {
     const [isEditModalOpen, setEditModalOpen] = useState(false);
     return (
         <Dialog open={isEditModalOpen} onOpenChange={setEditModalOpen}>
@@ -14,7 +19,11 @@ export const EditCategory = ({ category }: { category: Category }) => {
             <DialogContent>
                 <DialogHeader>
                     <DialogTitle className="mb-4">Edit Category</DialogTitle>
-                    <FormCategory category={category} onSuccess={() => setEditModalOpen(false)} />
+                    <FormCategory
+                        category={category}
+                        onSuccess={() => setEditModalOpen(false)}
+                        initialCategories={initialCategories}
+                    />
                 </DialogHeader>
             </DialogContent>
         </Dialog>
