261145d4c8d4ae1e05a3c1981b6c946d4ec7722b
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-08-30 16:09:41 -0500

    feat(payments): integrate CreatePayment component with accounts and categories in PaymentsClient

--- src/app/(dashboard)/payments/_components/PaymentsClient.tsx
+++ src/app/(dashboard)/payments/_components/PaymentsClient.tsx
@@ -1,16 +1,21 @@
 'use client';
 
+import { CreatePayment } from '@/app/(dashboard)/payments/_components/CreatePayment';
 import { PaymentsColum } from '@/app/(dashboard)/payments/_components/PaymentsColum';
 import { usePaymentsData } from '@/app/(dashboard)/payments/_lib/hooks';
 import type { Payment } from '@/app/(dashboard)/payments/_lib/types';
 import { DataTable } from '@/components/DataTable';
+import type { Account } from '@/type/Accounts';
+import type { Category } from '@/type/Categories';
 import type { ColumnDef } from '@tanstack/table-core';
 
 interface PaymentsClientProps {
     initialPayments: { data: Payment[] };
+    initialAccounts: { data: Account[] };
+    initialCategories: { data: Category[] };
 }
 
-export const PaymentsClient = ({ initialPayments }: PaymentsClientProps) => {
+export const PaymentsClient = ({ initialPayments, initialAccounts, initialCategories }: PaymentsClientProps) => {
     const { payments } = usePaymentsData({ initialPayments });
     const columns: ColumnDef<Payment>[] = PaymentsColum();
 
@@ -18,6 +23,9 @@ export const PaymentsClient = ({ initialPayments }: PaymentsClientProps) => {
         <section className="grid gap-12 w-full max-w-7xl mx-auto px-4">
             <div className="flex flex-wrap justify-between items-center gap-4">
                 <h1 className="text-2xl font-bold">Payments</h1>
+                <div className="flex items-center gap-4">
+                    <CreatePayment initialCategories={initialCategories} initialAccounts={initialAccounts} />
+                </div>
             </div>
             <div className="overflow-x-auto">
                 <DataTable columns={columns} data={payments.data} pageSize={22} />
